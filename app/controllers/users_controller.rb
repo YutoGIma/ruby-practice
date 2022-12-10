1a854f3fd0e17d6c16800e973602969742216288
GimaYuto <yurihanagima@gmail.com>
2022-12-10 14:26:19 +0900

    ユーザー新規登録実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,31 @@
 class UsersController < ApplicationController
+    # ユーザー一覧取得
     def index
         @users = User.all
     end
 
+    # ユーザーの詳細取得
     def show
         @user = User.find(params[:id])
     end
+
+    # ユーザー新規登録画面取得
+    def new
+        @user = User.new
+    end
+
+    # ユーザー新規登録
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to @user
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+        def user_params
+            params.require(:user).permit(:name, :email, :password, :explanation)
+        end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # ユーザー一覧取得
   get "/users", to: "users#index"
+  # ユーザー新規登録フォームを取得
+  get "/users/new", to: "users#new", as: :new_user
+  # ユーザー新規作成
+  post "users", to: "users#create"
+  # ユーザー詳細取得
   get "/users/:id", to: "users#show", as: :user
 end
