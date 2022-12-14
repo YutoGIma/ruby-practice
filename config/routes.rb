d8ecd9f8f120d34f9710222b979cfac1744a8ec9
GimaYuto <yurihanagima@gmail.com>
2022-12-14 13:51:49 +0900

    修正と入力部分共通化

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # ユーザー一覧取得
-  get "/users", to: "users#index"
-  # ユーザー新規登録フォームを取得
-  get "/users/new", to: "users#new", as: :new_user
-  # ユーザー新規作成
-  post "users", to: "users#create"
-  # ユーザー詳細取得
-  get "/users/:id", to: "users#show", as: :user
-  # ユーザー編集フォームを取得
-  get "/users/:id/edit", to: "users#edit", as: :edit_user
-  # ユーザー情報更新
-  patch "/users/:id", to: "users#update"
-  # ユーザーの削除
-  delete "/users/:id", to: "users#destroy"
+  resources :users
 end
