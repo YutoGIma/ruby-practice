76fb4de44fd2c4348716fe6a1197e7f8d32b54f6
YutoGIma <yurihanagima@gmail.com>
2023-05-21 03:57:18 +0900

    ユーザー新規作成機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,50 +5,50 @@ class UsersController < ApplicationController
         render json: @users
     end
 
-    # # ユーザーの詳細取得
-    # def show
-    #     @user = User.find(params[:id])
-    # end
+    # ユーザーの詳細取得
+    def show
+        @user = User.find(params[:id])
+    end
 
-    # # ユーザー新規登録画面取得
-    # def new
-    #     @user = User.new
-    # end
+    # ユーザー新規登録画面取得
+    def new
+        @user = User.new
+    end
 
-    # # ユーザー新規登録
-    # def create
-    #     @user = User.new(user_params)
-    #     if @user.save
-    #         redirect_to @user
-    #     else
-    #         render :new, status: :unprocessable_entity
-    #     end
-    # end
+    # ユーザー新規登録
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to @user
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
 
-    # # ユーザー編集画面取得
-    # def edit
-    #     @user = User.find(params[:id])
-    # end
+    # ユーザー編集画面取得
+    def edit
+        @user = User.find(params[:id])
+    end
 
-    # # ユーザー更新
-    # def update
-    #     @user = User.find(params[:id])
-    #     if @user.update(user_params)
-    #         redirect_to @user
-    #     else
-    #         render :new, status: :unprocessable_entity
-    #     end
-    # end
+    # ユーザー更新
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            redirect_to @user
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
 
-    # # ユーザーの削除
-    # def destroy
-    #     @user = User.find(params[:id])
-    #     @user.destroy
-    #     redirect_to users_path
-    # end
+    # ユーザーの削除
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        redirect_to users_path
+    end
 
-    # private
-    #     def user_params
-    #         params.require(:user).permit(:name, :email, :password, :explanation)
-    #     end
+    private
+        def user_params
+            params.require(:user).permit(:name, :email, :password, :explanation)
+        end
 end
